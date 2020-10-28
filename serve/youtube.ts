70226e2ead1df99081cc2a3fa217ffc3430c0a8c
Juan Avendaño Nuñez <javendano.n@gmail.com>
2020-10-27 22:22:57 -0300

    obtain y unitaryOBtain alpha

--- serve/youtube.ts
+++ serve/youtube.ts
@@ -1,10 +1,39 @@
 import { extractors } from "../src";
 import { Logger } from "../src/common/Logger";
 import { Youtube } from "../src/modules/Youtube";
+import { Readline } from "./Readline";
+const axios = require("axios");
 
+type content = {
+	apiKey: String;
+	passwd: String;
+}
+const config = {apiKey: "AIzaSyCbh7V9N99YuffN2s8xeu7MmfYS4l2I180", videoID: "C9ck7UKNSQg"}  //temporal
 const logger = new Logger("youtube-test");
 const youtubeE: Youtube = extractors.find((e) => e.register.id === "youtube-extractor") as any;
+
+
+async function oauth(){
+	let apiKey: String;
+	// validate apikey
+	while (!apiKey){
+		const in_apiKey = await Readline.read('Ingrese su API KEY de YouTube (Google)')
+		axios.get(`https://www.googleapis.com/youtube/v3/search?key=${in_apiKey}`)
+		.then((res : any) => {
+			// Apikey válida 
+			logger.log(res.status);
+			if (res.status==200){
+				apiKey = in_apiKey;
+			}
+		})
+		.catch((err: any) => {
+			// error c murió
+			logger.error('APIKey inválida');
+		})
+	}
+}
 async function main() {
+	await oauth();
 	return process.exit(0);
 }
 try {
@@ -13,3 +42,4 @@ try {
 	logger.error("[MAIN ERROR]", error);
 	process.exit(1);
 }
+
--- src/modules/Youtube/index.ts
+++ src/modules/Youtube/index.ts
@@ -1,5 +1,6 @@
 import { Extractor } from "../Extractor";
 import { Response } from "../Extractor/Response";
+const axios = require("axios");
 
 export class Youtube extends Extractor {
 	constructor() {
@@ -10,12 +11,34 @@ export class Youtube extends Extractor {
 		});
 	}
 	async deploy(config: Youtube.Deploy.Config, options: Youtube.Deploy.Options): Promise<Response> {
+
 		return new Response(this, Response.Status.OK);
 	}
 	async obtain(options: Youtube.Obtain.Options): Promise<Response> {
+
+		let tokenPage = "";
+		while (tokenPage){
+			
+		// obtiene toda la info de un video con el ID y una apikey 
+			axios.get(`https://www.googleapis.com/youtube/v3/commentThreads?key=${options.apiKey}&textFormat=plainText&part=snippet&videoId=${options.videoID}&pageToken=${tokenPage}`)
+			.then((res: any)=>{
+				// +20 results?
+				tokenPage = res.data.nextPageToken
+				res.data.items.map((comment) => options.comments.push(comment.snippet.topLevelComment.snippet.textOriginal))
+				// +20
+			})
+			.catch(err => {
+				this.logger.error(err)
+			})
+		}
 		return new Response(this, Response.Status.OK);
 	}
 	async unitaryObtain(options: Youtube.UnitaryObtain.Options): Promise<Response> {
+
+		//get a comment by id 
+		axios.get(`https://www.googleapis.com/youtube/v3/comments?part=snippet&id={COMMENT_ID}&textFormat=html&key=${options.}`)
+		https://www.googleapis.com/youtube/v3/comments?part=snippet&id={COMMENT_ID}&textFormat=html&key={YOUR_API_KEY} 
+
 		return new Response(this, Response.Status.OK);
 	}
 	async destroy(options: Youtube.Destroy.Options): Promise<Response> {
@@ -24,16 +47,24 @@ export class Youtube extends Extractor {
 }
 export namespace Youtube {
 	export namespace Deploy {
-		export interface Config extends Extractor.Deploy.Config {}
+		export interface Config extends Extractor.Deploy.Config {
+		}
 		export interface Options extends Extractor.Deploy.Options {}
 		export interface Response extends Extractor.Deploy.Response {}
 	}
 	export namespace Obtain {
-		export interface Options extends Extractor.Obtain.Options {}
+		export interface Options extends Extractor.Obtain.Options {
+			apiKey: string;
+			videoID: String;
+			comments: [String];
+		}
 		export interface Response extends Extractor.Obtain.Response {}
 	}
 	export namespace UnitaryObtain {
-		export interface Options extends Extractor.UnitaryObtain.Options {}
+		export interface Options extends Extractor.UnitaryObtain.Options {
+			apiKey: string;
+			commentID: string;
+		}
 		export interface Response extends Extractor.UnitaryObtain.Response {}
 	}
 	export namespace Destroy {
