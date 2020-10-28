5cdb2fad20cec83bcece16495669a7295b416711
Juan Avendaño Nuñez <javendano.n@gmail.com>
2020-10-28 00:19:01 -0300

    merge update

--- src/modules/Youtube/index.ts
+++ src/modules/Youtube/index.ts
@@ -3,10 +3,9 @@ import { Analyzer } from "@/Analyzer";
 import Axios, { AxiosInstance } from "axios";
 import { Extractor } from "../Extractor";
 import { Response } from "../Extractor/Response";
+
 export class Youtube extends Extractor {
-	/*
 	private api: AxiosInstance; // En caso de instanciar desde deploy remover readonly
-    */
 	constructor() {
 		super({
 			id: "youtube-extractor", // Identificador, solo letras minúsculas y guiones (az-)
@@ -15,7 +14,14 @@ export class Youtube extends Extractor {
 		});
 	}
 	async deploy(config: Youtube.Deploy.Config, options: Youtube.Deploy.Options): Promise<Response> {
-
+		this.api = Axios.create({
+			baseURL: "https://www.googleapis.com/youtube/v3/", // Base URL,
+			responseType: "json",
+			headers: {
+				// Se añade la api key en el header
+				"api-key": config.apiKey,
+			},
+		});
 		return new Response(this, Response.Status.OK);
 	}
 	async obtain(options: Youtube.Obtain.Options): Promise<Response> {
@@ -35,40 +41,39 @@ export class Youtube extends Extractor {
 		// 		this.logger.error(err)
 		// 	})
 		// }
-		/*
+
         // Se crea instancia de axios con el endpoint de la api
 		// https://github.com/axios/axios#axios-api
-		this.api = Axios.create({
-			baseURL: "https://api.test", // Base URL,
-			responseType: "json",
-			headers: {
-				// Se añade la api key en el header
-				"api-key": config.apiKey,
-			},
-		});
-        */
 
-		/*
         const analyzer = new Analyzer(this);
-		// request del tipo post
-		const response = await this.api.post<{ messages: string[] }>("/api/TEST", {
-			postParam: 123,
-			postParam2: "asd",
-		});
-		const message: Analyzer.input[] = response.data.messages.map((content) => ({ content }));
+		const response = await this.api.get<{ ytData: JSON }>("/commentThreads", {params: {
+			key: options.apiKey,
+			videoId: options.metaKey,
+			part: options.part
+		}});
+		if (options.part=='snippet'){
+			const message: Analyzer.input[] = response.data.ytData.items.map((comment : JSON) => ({ content: comment.snippet.topLevelComment.snippet.textOriginal}));
+		} // else if (options=='snippet,replies'){
+			//
+		//}
 		const analysis = await analyzer.analyze(message);
+
 		return new Response<Analyzer.Analysis>(this, Response.Status.OK, analysis);
-        */
-
-
-		return new Response(this, Response.Status.OK);
 	}
 	async unitaryObtain(options: Youtube.UnitaryObtain.Options): Promise<Response> {
 
 		//get a comment by id 
 		// axios.get(`https://www.googleapis.com/youtube/v3/comments?part=snippet&id={COMMENT_ID}&textFormat=html&key=${options.}`)
 		// https://www.googleapis.com/youtube/v3/comments?part=snippet&id={COMMENT_ID}&textFormat=html&key={YOUR_API_KEY} 
+		const analyzer = new Analyzer(this);
 
+		const response = await this.api.get<{ ytData: JSON }>("/comments", {params: {
+			key: options.apiKey,
+			id: options.metaKey,  // commentId
+			part: options.part,
+		}});
+		
+		const analysis = await analyzer.analyze(message)
 		return new Response(this, Response.Status.OK);
 	}
 	async destroy(options: Youtube.Destroy.Options): Promise<Response> {
@@ -78,9 +83,7 @@ export class Youtube extends Extractor {
 export namespace Youtube {
 	export namespace Deploy {
 		export interface Config extends Extractor.Deploy.Config {
-			/*
             apiKey: string;
-            */
 		}
 		export interface Options extends Extractor.Deploy.Options {}
 		export interface Response extends Extractor.Deploy.Response {}
@@ -88,15 +91,16 @@ export namespace Youtube {
 	export namespace Obtain {
 		export interface Options extends Extractor.Obtain.Options {
 			apiKey: string;
-			videoID: String;
 			comments: [String];
+			// snippet para top level comentario, "snippet,replies" para top level comentarios y respuestas de los comentarios
+			part: String;
 		}
 		export interface Response extends Extractor.Obtain.Response {}
 	}
 	export namespace UnitaryObtain {
 		export interface Options extends Extractor.UnitaryObtain.Options {
 			apiKey: string;
-			commentID: string;
+			part: String;
 		}
 		export interface Response extends Extractor.UnitaryObtain.Response {}
 	}
