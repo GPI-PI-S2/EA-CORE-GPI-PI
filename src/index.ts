e7a31004893a23f8e156341b4cce21311dce6eab
Juan Avendaño Nuñez <javendano.n@gmail.com>
2020-11-02 22:48:31 -0300

    fixed error por merge

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,7 @@ import { Emol } from "./modules/Emol";
 import { Extractor } from "./modules/Extractor";
 import { Reddit } from "./modules/Reddit";
 import { Telegram } from "./modules/Telegram";
-<<<<<<< HEAD
-import { Youtube } from './modules/Youtube'
-
-export const extractors: Extractor[] = [new Telegram(), new Youtube()];
-=======
 import { Twitter } from "./modules/Twitter";
 import { Youtube } from "./modules/Youtube";
 
 export const extractors: Extractor[] = [new Telegram(), new Emol(), new Reddit(), new Twitter(), new Youtube()];
->>>>>>> 29b76dbb9610e07f78eaafdb341bbeccd0964397
