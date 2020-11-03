7e869f6c4e411c1f7e75a74d9d6f7e06484962ec
Sebastian <sebastian.perezb@utem.cl>
2020-11-03 17:42:54 -0300

    simplificado obtencion de comentario, TODO incluir Oauth si es necesario

--- src/modules/Reddit/index.ts
+++ src/modules/Reddit/index.ts
@@ -4,21 +4,6 @@ import {log} from "util";
 import {Extractor} from "../Extractor";
 import {Response} from "../Extractor/Response";
 
-const flattenCommentsTree = (nodo: any): string[] => {
-	if (nodo.kind === "Listing") {
-		return nodo.data.children.flatMap((children: any) => flattenCommentsTree(children));
-	}
-	if (nodo.kind === "t1") {
-		if (nodo.data.replies.kind === "Listing") {
-			let rest = flattenCommentsTree(nodo.data.replies);
-			rest.unshift(nodo.data.body as string);
-			return rest;
-		}
-		return [nodo.data.body as string];
-	}
-	return [];
-}
-
 export class Reddit extends Extractor {
 	private api: AxiosInstance; // En caso de instanciar desde deploy remover readonly
 	constructor() {
@@ -30,20 +15,25 @@ export class Reddit extends Extractor {
 	}
 	async deploy(config: Reddit.Deploy.Config, options: Reddit.Deploy.Options): Promise<Response<unknown>> {
 		this.api = Axios.create({
-			baseURL: "https://www.reddit.com/r/", // Base URL,
+			baseURL: "https://www.reddit.com", // Base URL,
 			responseType: "json",
 		});
 		return new Response(this, Response.Status.OK);
 	}
-	async obtain(options: Reddit.Obtain.Options): Promise<Response<unknown>> {
-		const {limit, minSentenceSize, subReddit, postId} = options;
-		this.logger.log("Obtaining: SubReddit: ", subReddit, "Post Id: ", postId);
-		try {
-			const response = await this.api.get(`${subReddit}/comments/${postId}.json`, {params: {limit: limit}});
-			this.logger.log("Request status: ", response.status);
-			const comments = flattenCommentsTree(response.data[1]);
 
-			this.logger.log("Comments read: ", comments.length);
+	private getCommentsList(commentsData: any): string[] {
+		return commentsData.map((commentData: any) => commentData.data.body)
+	}
+
+	async obtain(options: Reddit.Obtain.Options): Promise<Response<unknown>> {
+		// oauth Reddit 0XGi1M9cPjNx1oAmjp51n0PLPlaSPg
+		const {limit, minSentenceSize, subReddit, postId} = options;
+		const subRedditParam = subReddit ? `/r/${subReddit}` : '';
+		this.logger.log("Obtaining: SubReddit: ", subReddit, ", Post Id: ", postId);
+		try {
+			const response = await this.api.get(`${subRedditParam}/comments/${postId}.json`, {params: {limit: limit, threaded: false}});
+			this.logger.log("Request status: ", response.status);
+			const comments = this.getCommentsList(response.data[1].data.children);
 			const analyzer = new Analyzer(this);
 			const messages: Analyzer.input[] = comments.map(content => ({content}));
 			const filteredMessages = messages.filter(message => Analyzer.filter(message, {minSentenceSize}));
@@ -76,8 +66,8 @@ export namespace Reddit {
 	}
 	export namespace Obtain {
 		export interface Options extends Extractor.Obtain.Options {
-			subReddit: String,
-			postId: String,
+			subReddit: string,
+			postId: string,
 		}
 		export interface Response extends Extractor.Obtain.Response {}
 	}
