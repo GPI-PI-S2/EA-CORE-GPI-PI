8d651bd3d90ae6a303f58818f5a2385f2a660641
VRAraya <victor.arayar@utem.cl>
2020-11-06 23:19:30 -0300

    Agregando la funcion deploy y la funcion obtain

--- serve/twitter.ts
+++ serve/twitter.ts
@@ -1,10 +1,47 @@
+import { read } from "fs";
 import { extractors } from "../src";
 import { Logger } from "../src/common/Logger";
 import { Twitter } from "../src/modules/Twitter";
+import { Readline } from "./Readline";
 
 const logger = new Logger("twitter-test");
 const twitterE: Twitter = extractors.find((e) => e.register.id === "twitter-extractor") as any;
+
+
+// async function main() {
+// 	const loginResponse = await login();
+// 	const selectedChat = await selectChat(loginResponse.chats);
+// 	const { accessHash, id, type } = selectedChat;
+// 	await telegramE.obtain({
+// 		accessHash,
+// 		type,
+// 		chatId: id,
+// 		limit: 50,
+// 		metaKey: JSON.stringify(selectedChat),
+// 		minSentenceSize: 2,
+// 	});
+// 	process.exit(0);
+// }
 async function main() {
+	logger.log("---Deploy---");
+	twitterE.deploy({
+		bearerToken : 'AAAAAAAAAAAAAAAAAAAAAJewJQEAAAAAPOlJ%2BAGXOMiIAG7dDUUtTaFAOgs%3DKLpEMQCsq33R2kwwnADok1ujE9v65o4eSf34m5b4yupPvGCi40'
+	}, {});
+
+	let hashtag:string;	
+	let max_results:number = 100;
+	while(!hashtag) {
+		hashtag = await Readline.read("Para buscar ingrese un hashtag:");
+
+	}
+	
+	// = await Readline.read("Ingrese la cantidad máxima de tweets a obtener (puede elegir entre 1 y 100):");
+	await twitterE.obtain({
+		hashtag: hashtag,
+		metaKey: "",
+		limit: max_results,
+		minSentenceSize: 3
+	});
 	return process.exit(0);
 }
 try {
--- src/modules/Twitter/index.ts
+++ src/modules/Twitter/index.ts
@@ -1,14 +1,13 @@
-/*
+
 import { Analyzer } from "@/Analyzer";
 import Axios, { AxiosInstance } from "axios";
-*/
 import { Extractor } from "../Extractor";
 import { Response } from "../Extractor/Response";
 
 export class Twitter extends Extractor {
-	/* 	
+	
     private api: AxiosInstance; // En caso de instanciar desde deploy remover readonly
-	 */
+	
 	constructor() {
 		super({
 			id: "twitter-extractor", // Identificador, solo letras minúsculas y guiones (az-)
@@ -17,33 +16,54 @@ export class Twitter extends Extractor {
 		});
 	}
 	async deploy(config: Twitter.Deploy.Config, options: Twitter.Deploy.Options): Promise<Response<unknown>> {
-		/*
-		// Se crea instancia de axios con el endpoint de la api
-		// https://github.com/axios/axios#axios-api
-		this.api = Axios.create({
-			baseURL: "https://api.test", // Base URL,
+		// bearerToken = AAAAAAAAAAAAAAAAAAAAAJewJQEAAAAAPOlJ%2BAGXOMiIAG7dDUUtTaFAOgs%3DKLpEMQCsq33R2kwwnADok1ujE9v65o4eSf34m5b4yupPvGCi40
+
+	   	this.api = Axios.create({
+			baseURL: 'https://api.twitter.com/2/tweets',
 			responseType: "json",
 			headers: {
-				// Se añade la api key en el header
-				"api-key": config.apiKey,
-			},
-        });
-        */
+				'Authorization': 'Bearer '+ config.bearerToken
+			}
+	  	});
+		this.logger.log('Deployed.')
 		return new Response(this, Response.Status.OK);
 	}
 	async obtain(options: Twitter.Obtain.Options): Promise<Response<unknown>> {
-		const { limit, metaKey, minSentenceSize } = options;
 		/*
-		const analyzer = new Analyzer(this);
-		// request del tipo post
-		const response = await this.api.post<{ messages: string[] }>("/api/TEST", {
-			postParam: 123,
-			postParam2: "asd",
-		});
-		const message: Analyzer.input[] = response.data.messages.map((content) => ({ content }));
-		const analysis = await analyzer.analyze(message);
-        return new Response<Analyzer.Analysis>(this, Response.Status.OK, analysis);
-        */
+		Función para obtener una lista de Tweets, el limite máximo de tweets es de 100
+		*/
+		const { hashtag, limit, metaKey, minSentenceSize} = options;
+		const query = '#'+ hashtag
+		this.logger.debug("Searching by hashtag: ", query);
+
+		try {
+			const response = await this.api.get(
+				'/search/recent', {
+					params: {
+						max_results: limit,
+						query: query
+					}
+				});
+			this.logger.log("Request status: ", response.status);
+			const tweets = response.data.data
+			const tweetsTexts = tweets.map(function (tweet:any) {
+				return tweet.text; 
+			});
+						
+			const analyzer = new Analyzer(this);
+			const tweetsTxts: Analyzer.input[] = tweetsTexts.map((content: any) => ({content}));
+			this.logger.log("Tweet's Content Length: ", tweetsTxts);
+			const filteredTweets = tweetsTxts.filter(tweet => Analyzer.filter(tweet, {minSentenceSize}));
+			this.logger.log("Valid tweets: ", filteredTweets.length);
+			const analysis = await analyzer.analyze(filteredTweets);
+			this.logger.log("Analysis: ", analysis);
+			return new Response<Analyzer.Analysis>(this, Response.Status.OK, analysis);
+		}
+		catch (error) {
+			this.logger.error(error.response);
+			return new Response<Analyzer.Analysis>(this, Response.Status.ERROR);
+		}
+        
 		return new Response(this, Response.Status.OK);
 	}
 	async unitaryObtain(options: Twitter.UnitaryObtain.Options): Promise<Response<unknown>> {
@@ -56,15 +76,15 @@ export class Twitter extends Extractor {
 export namespace Twitter {
 	export namespace Deploy {
 		export interface Config extends Extractor.Deploy.Config {
-			/*
-            apiKey: string;
-            */
+            bearerToken: string;
 		}
 		export interface Options extends Extractor.Deploy.Options {}
 		export interface Response extends Extractor.Deploy.Response {}
 	}
 	export namespace Obtain {
-		export interface Options extends Extractor.Obtain.Options {}
+		export interface Options extends Extractor.Obtain.Options {
+			hashtag:string;
+		}
 		export interface Response extends Extractor.Obtain.Response {}
 	}
 	export namespace UnitaryObtain {
